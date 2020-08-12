0daef089d3ddcf5ca6c4e8ab678e4110be34301f
Foestauf <46908845+foestauf@users.noreply.github.com>
2020-08-12 11:04:09 -0400

    Removed reaction confirmation. Bot no longer attempts to set nickname if already set.

--- vatstar.js
+++ vatstar.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 const axios = require("axios").default;
 
-const { prefix, token } = require("./config.json");
+const { prefix, token, channelId } = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,120 +11,131 @@ client.on("ready", () => {
 
 client.on("message", async (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  const args = message.content.slice(prefix.length).trim().split(" ");
-  const command = args.shift().toLowerCase();
-  let response = {};
+  if (message.channel.name === channelId) {
+    const args = message.content.slice(prefix.length).trim().split(" ");
+    const command = args.shift().toLowerCase();
+    let response = {};
 
-  if (command === "avengers_assemble") {
-    let count = 0;
+    if (command === "avengers_assemble") {
+      console.log(botChannel);
+
+      let count = 0;
       let memberRole = message.member.guild.roles.cache.find(
         (role) => role.name === "Member"
       );
-      message.guild.members.cache.filter(m => !m.user.bot).forEach(member => {
-        member.roles.add(memberRole);
-        count += 1;
-      }
-        );
-        message.reply(`Operation complete. Assigned the member role to ${count}`)
-        
-
-  }
-
-  if (message.content === "!ping") {
-    message.reply("Pong!");
-  } else if (command === "vatstar") {
-    console.log(`User ${message.member} is paging us`);
-    if (!args.length) {
-      return message.reply(
-        `Please respond in the format of "!vatstar 1234567" with your VATSIM ID`
-      );
-    }
-    console.log(`Command name: ${command}\nArguments: ${args}`);
-    try {
-      const res = await axios
-        .get(`https://api.vatsim.net/api/ratings/${args[0]}/`)
-        .then((data) => {
-          response = data;
-          const {
-            id,
-            rating,
-            pilotrating: pilotRating,
-            name_first,
-            name_last,
-          } = response.data;
-          let full_name = name_first + " " + name_last;
-          // Start await reactions here
-          message.react("👍").then(() => message.react("👎"));
-          const filter = (reaction, user) => {
-            return (
-              ["👍", "👎"].includes(reaction.emoji.name) &&
-              user.id === message.author.id
-            );
-          };
-          message.reply(
-            `I have found ${full_name}, is that right? Please react to your original message with either 👍 for yes or 👎 for no.`
-          );
-
-          message
-            .awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] })
-            .then((collected) => {
-              const reaction = collected.first();
-
-              if (reaction.emoji.name === "👍") {
-                message.member
-                  .setNickname(full_name)
-                  .then((res) => {})
-                  .catch((err) => console.log(err));
-                message.reply("Great, I will adjust your nickname for you.");
-                console.log(`Pilot rating is ${pilotRating}`);
-                let newRoles = roleSelector(message, pilotRating);
-                if (rating > 1)
-                  newRoles.push(
-                    message.member.guild.roles.cache.find(
-                      (role) => role.name === "Controllers"
-                    )
-                  );
-                const member = message.mentions.members.first();
-                message.member.roles.add(newRoles);
-                let rolesString = newRoles.join();
-                if (rolesString.length > 0) {
-                  message.reply(
-                    `You have been assigned the following roles : ${rolesString}`
-                  );
-                } else {
-                  let studentRole = message.member.guild.roles.cache.find(
-                    (role) => role.name === "Student"
-                  );
-                  message.reply(`Welcome to VATSTAR. As you currently do not have any pilot ratings you have automatically been given the role of ${studentRole}`);
-                }
-              } else {
-                message.reply('Okay we got that wrong, please check your vatsim ID number  and try again or contact staff for further assistance');
-              }
-            })
-            .catch((collected) => {
-              message.reply(
-                "you reacted with neither a thumbs up, nor a thumbs down."
-              );
-            });
-          // END reaction await
-
-          if (!message.guild.me.hasPermission("MANAGE_NICKNAMES"))
-            return message.channel.send(
-              "I do not have permission to adjust nickname"
-            );
+      message.guild.members.cache
+        .filter((m) => !m.user.bot)
+        .forEach((member) => {
+          member.roles.add(memberRole);
+          count += 1;
         });
-    } catch (error) {
-      console.log(error);
-      if (error.response.status === 404) {
-        message.reply(`VATSIM ID "${args[0]}" not found`);
+      message.reply(`Operation complete. Assigned the member role to ${count}`);
+    }
+
+    if (message.content === "!ping") {
+      message.reply("Pong!");
+    } else if (command === "vatstar") {
+      console.log(`User ${message.member} is paging us`);
+      if (!args.length) {
+        return message.reply(
+          `Please respond in the format of "!vatstar 1234567" with your VATSIM ID`
+        );
+      }
+      console.log(`Command name: ${command}\nArguments: ${args}`);
+      try {
+        const res = await axios
+          .get(`https://api.vatsim.net/api/ratings/${args[0]}/`)
+          .then((data) => {
+            response = data;
+            const {
+              id,
+              rating,
+              pilotrating: pilotRating,
+              name_first,
+              name_last,
+            } = response.data;
+            let full_name = name_first + " " + name_last;
+            if (!message.guild.me.hasPermission("MANAGE_NICKNAMES"))
+              return message.channel.send(
+                "I do not have permission to adjust nickname"
+              );
+
+            // Start await reactions here
+            // message.react("👍").then(() => message.react("👎"));
+            // const filter = (reaction, user) => {
+            //   return (
+            //     ["👍", "👎"].includes(reaction.emoji.name) &&
+            //     user.id === message.author.id
+            //   );
+            // };
+            // message.reply(
+            //   `I have found ${full_name}, is that right? Please react to your original message with either 👍 for yes or 👎 for no.`
+            // );
+
+            // message
+            //   .awaitReactions(filter, { max: 1, time: 60000, errors: ["time"] })
+            //   .then((collected) => {
+            //     const reaction = collected.first();
+
+            //     if (reaction.emoji.name === "👍") {
+            if (!message.member.nickname === full_name) {
+              console.log(message.member.nickname);
+              console.log(full_name);
+              message.reply(
+                `Hello ${full_name}, I will adjust your nickname for you.`
+              );
+
+              message.member
+                .setNickname(full_name)
+                .then((res) => {})
+                .catch((err) => console.log(err));
+            }
+
+            console.log(`Pilot rating is ${pilotRating}`);
+            let newRoles = roleSelector(message, pilotRating);
+            if (rating > 1)
+              newRoles.push(
+                message.member.guild.roles.cache.find(
+                  (role) => role.name === "Controllers"
+                )
+              );
+            const member = message.mentions.members.first();
+            message.member.roles.add(newRoles);
+            let rolesString = newRoles.join();
+            if (rolesString.length > 0) {
+              message.reply(
+                `You have been assigned the following roles : ${rolesString}`
+              );
+            } else {
+              let memberRole = message.member.guild.roles.cache.find(
+                (role) => role.name === "Member"
+              );
+              message.reply(
+                `Welcome to VATSTAR. As you currently do not have any pilot ratings you have automatically been given the role of ${memberRole}`
+              );
+            }
+            // } else {
+            //   message.reply('Okay we got that wrong, please check your vatsim ID number  and try again or contact staff for further assistance');
+            // }
+            // })
+            // .catch((collected) => {
+            //   message.reply(
+            //     "you reacted with neither a thumbs up, nor a thumbs down."
+            //   );
+            // });
+            // END reaction await
+          });
+      } catch (error) {
+        console.log(error);
+        if (error.response.status === 404) {
+          message.reply(`VATSIM ID "${args[0]}" not found`);
+        }
       }
     }
   }
 });
 
 function roleString(roles) {
-  console.log("roles list is" + roles);
-  console.log(roles.length);
   if (roles.length === 0) return false;
   else {
     return true;
@@ -133,24 +144,23 @@ function roleString(roles) {
 
 function roleSelector(message, rating) {
   let roles = [];
-  console.log(`Rating in selector is :${rating}`);
   const roleSymbol = findRoles(message);
   if ((rating & 16) === 16) {
-    roles.push(roleSymbol.p5)
+    roles.push(roleSymbol.p5);
   }
-   if ((rating & 8) === 8) {
-    roles.push(roleSymbol.p4)
-  } 
-   if ((rating & 4) === 4) {
+  if ((rating & 8) === 8) {
+    roles.push(roleSymbol.p4);
+  }
+  if ((rating & 4) === 4) {
     roles.push(roleSymbol.p3);
   }
-   if ((rating & 2) === 2) {
+  if ((rating & 2) === 2) {
     roles.push(roleSymbol.p2);
-  } 
+  }
   if ((rating & 1) === 1) {
     roles.push(roleSymbol.p1);
-  } 
-  
+  }
+
   return roles;
 }
 
