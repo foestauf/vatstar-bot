27aee84d615b5655b90d833feb80f94519706987
Foestauf <foestauf@gmail.com>
2020-09-02 20:02:40 -0400

    docker uses service name as hostname. derp.

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import mongoose = require('mongoose');
 import { GuildMember } from 'discord.js';
-mongoose.connect('mongodb://172.19.0.3/vatstar', {
+mongoose.connect('mongodb://database/vatstar', {
     user: "mongodb",
     pass: "mongodb",
     useNewUrlParser: true,
--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateUser = exports.retrieveUser = exports.newUser = void 0;
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://172.19.0.3/vatstar', {
+mongoose.connect('mongodb://database/vatstar', {
     user: "mongodb",
     pass: "mongodb",
     useNewUrlParser: true,
