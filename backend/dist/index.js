ff2a17e118fa025f1f4b5b7b0131d38ecd2ca932
foestauf@gmail.com <foestauf@DESKTOP-LJGI22A.localdomain>
2020-09-27 17:37:49 -0400

    Added frontend to docker-compose. Configuration for backend.

---
+++ backend/dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+require("dotenv/config");
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const db = process.env.MONGO_URI;
+mongoose_1.default
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connection successful'))
+    .catch((err) => console.log(err));
+const APP_PORT = 4000;
+const app = express_1.default();
+app.use(morgan_1.default('combined'));
+app.use(cors_1.default());
+// app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({
+    extended: true,
+}));
---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import 'dotenv/config';
+import morgan from 'morgan';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+// DB config
+interface ProcessEnv {
+    [MONGO_URI: string]: string
+}
+
+const db: string = (process.env.MONGO_URI as string);
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connection successful'))
+    .catch((err) => console.log(err));
+
+
+const APP_PORT = 4000;
+const app = express();
+
+app.use(morgan('combined'));
+app.use(cors());
+// app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
