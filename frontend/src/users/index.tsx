67ed2f16503e7853d7403e72c5e87acf751cecdf
Foestauf <foestauf@gmail.com>
2020-12-26 10:05:54 -0500

    Modal Creation

--- frontend/src/users/index.tsx
+++ frontend/src/users/index.tsx
@@ -1,10 +1,48 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable react/prop-types */
 import axios, { AxiosResponse } from 'axios';
-import React, { FunctionComponent } from 'react';
-import { Table } from 'react-bootstrap';
+import React, { FunctionComponent, useState } from 'react';
+import { Button, Modal, Table } from 'react-bootstrap';
 import { useQuery } from 'react-query';
 // Using require statement until I can resolve type errors as an import
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const lodash = require('lodash');
+
+interface ModalProps {
+  onHide: () => void;
+  show: boolean;
+  name: string;
+  vatsimId: string;
+}
+
+const UserModal = (props: ModalProps): JSX.Element => {
+  return (
+    <Modal
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            VatSIM user details
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>User</h4>
+          <p>This is my modal body</p>
+          <p>Username: {props.name}</p>
+          <p>Vat Sim ID: {props.vatsimId}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </>
+    </Modal>
+  );
+};
 
 export const UserList: FunctionComponent = () => {
   interface UserValue {
@@ -31,7 +69,7 @@ export const UserList: FunctionComponent = () => {
   };
 
   const { isLoading, isError, data, error } = useQuery('users', fetchUsers);
-
+  const [modalShow, setModalShow] = useState(false);
   return (
     <div>
       <Table>
@@ -61,11 +99,20 @@ export const UserList: FunctionComponent = () => {
               }
               return (
                 <tr key={value._id}>
-                  <td>{value.vatsimId}</td>
+                  <Button variant="primary" onClick={() => setModalShow(true)}>
+                    <td>{value.vatsimId}</td>
+                  </Button>
+
                   <td>{value.name}</td>
                   <td>{ratings}</td>
 
                   <td>{value.lastSeen}</td>
+                  <UserModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                    name={value.name}
+                    vatsimId={value.vatsimId}
+                  />
                 </tr>
               );
             })
